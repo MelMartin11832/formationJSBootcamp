a3ec1c2e50aaa98f8c48ec23a9b8c4450098e2c0
Nicolas Laval <nicolas.laval@insee.fr>
2018-01-26 14:16:19 +0100

    Refactor page-title component

--- src/js/components/shared/page-title/page-title.jsx
+++ src/js/components/shared/page-title/page-title.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './page-title.css';
 
 function PageTitle({ title }) {
 	return (
 		<div className="row">
 			<div className="col-md-10 centered col-md-offset-1">
-				<h1>{title}</h1>
+				<h1 className="page-title">{title}</h1>
 			</div>
 		</div>
 	);
