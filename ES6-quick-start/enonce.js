3f6f40fb5ff033753f7fe61d28d3ff0289c1d039
Olivier Levitt <olivier.levitt@insee.fr>
2018-02-05 11:51:03 +0000

    Update enonce.js

--- ES6-quick-start/enonce.js
+++ ES6-quick-start/enonce.js
@@ -10,23 +10,23 @@ console.log(players);
 
 // Ex 1 : Ajouter un identifiant à chaque joueur
 const addId = TODO
-console.log("Ex1 : ", addId);
+console.log("Ex1 : ", "addId");
 
 // Ex 2 : Retourner le nom d'un joueur en fonction de son nom, 'NOT_EXIST' sinon
 const numPlayer = TODO
-console.log("Ex2 : ", numPlayer(1));
-console.log("Ex2 : ", numPlayer(10));
+console.log("Ex2 : ", "numPlayer(1)");
+console.log("Ex2 : ", "numPlayer(10)");
 
 // Ex 3 : Retourner un tableau contenant les noms de joueurs dont le numéro
 // est inférieur à 10
 const infTen = TODO
-console.log("Ex 3 : ", infTen);
+console.log("Ex 3 : ", "infTen");
 
 // Ex 4 : Calculer la moyenne d'âge des joueurs
 const ageAve = TODO
-console.log("Ex 4 : ", ageAve);
+console.log("Ex 4 : ", "ageAve");
 
 // Ex 5 : Afficher l'ensemble des attributs du Xème joueur du tableau, 'NOT_EXIST' sinon
 const attr = TODO
-console.log("Ex 5 : ", attr(2));
-console.log("Ex 5 : ", attr(5));
+console.log("Ex 5 : ","attr(2)");
+console.log("Ex 5 : ", "attr(5)");
