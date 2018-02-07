61964dd0a802e891598caceca22d286f20b0fab3
Mélanie Martin <melanie.martin@insee.fr>
2018-02-07 15:58:16 +0100

    debut ES6

--- ES6-quick-start/enonce.js
+++ ES6-quick-start/enonce.js
@@ -9,24 +9,25 @@ const players = [
 console.log(players);
 
 // Ex 1 : Ajouter un identifiant à chaque joueur
-const addId = TODO
-console.log("Ex1 : ", "addId");
+const addId = players.map((x, i) => ({ ...x, identifiant: i }));
+console.log("Ex1 : ", addId);
 
-// Ex 2 : Retourner le nom d'un joueur en fonction de son nom, 'NOT_EXIST' sinon
-const numPlayer = TODO
-console.log("Ex2 : ", "numPlayer(1)");
-console.log("Ex2 : ", "numPlayer(10)");
+// Ex 2 : Retourner le nom d'un joueur en fonction de son numero de jouer, 'NOT_EXIST' sinon
+const numPlayer = numeroDuJoueur => players.find(joueur => joueur.number === numeroDuJoueur);
+
+console.log("Ex2 : ", numPlayer(1));
+console.log("Ex2 : ", numPlayer(10));
 
 // Ex 3 : Retourner un tableau contenant les noms de joueurs dont le numéro
 // est inférieur à 10
-const infTen = TODO
+const infTen = TODO;
 console.log("Ex 3 : ", "infTen");
 
 // Ex 4 : Calculer la moyenne d'âge des joueurs
-const ageAve = TODO
+const ageAve = TODO;
 console.log("Ex 4 : ", "ageAve");
 
 // Ex 5 : Afficher l'ensemble des attributs du Xème joueur du tableau, 'NOT_EXIST' sinon
-const attr = TODO
-console.log("Ex 5 : ","attr(2)");
+const attr = TODO;
+console.log("Ex 5 : ", "attr(2)");
 console.log("Ex 5 : ", "attr(5)");
